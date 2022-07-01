b5597c818a2d49d881f4aa27a7198bd928f54b4a
JhinalRajput <jhinalnrajput2000@gmail.com>
2022-07-01 06:43:12 +0530

    changes made to the project

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from unicodedata import name
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.signup, name="signup"),
+    path('/', views.signup, name="signup"),
     path('signin', views.signin, name="signin"),
     path('logout',views.logout, name="logout"),
     path('error', views.error, name="error")
